becb07d825f3c7119dfda94670cff7f7cbf97bbd
Ahmed <ahmedmohamedaneng@gmail.com>
2023-12-06 04:38:21 +0200

    Modify

--- CI-CD/jenkins/myapp/hello.py
+++ CI-CD/jenkins/myapp/hello.py
@@ -1,9 +1,7 @@
-# import fire
+import fire
 
-# def hello(name="World"):
-#   return "Hello %s!" % name
+def hello(name="World"):
+  return "Hello %s!" % name
 
-# if __name__ == '__main__':
-#   fire.Fire(hello)
-
-print("Hello world from inside the app")
+if __name__ == '__main__':
+  fire.Fire(hello)
